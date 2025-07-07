601291537238364b753f9e7a50badc0e08c0c2f9
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-07-07 09:00:56 -0300

    first commit

---
+++ api/src/main/kotlin/com/ricky/hortinha/HortinhaApplication.kt
@@ -0,0 +1,11 @@
+package com.ricky.hortinha
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HortinhaApplication
+
+fun main(args: Array<String>) {
+	runApplication<HortinhaApplication>(*args)
+}
---
+++ api/src/test/kotlin/com/ricky/hortinha/HortinhaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ricky.hortinha
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HortinhaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
