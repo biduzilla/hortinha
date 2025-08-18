07de828ed4c16f847b8f98ce4075dbfb5b9108bc
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-08-18 10:41:08 -0300

    user crud

--- internal/data/users.go
+++ internal/data/users.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"hortinha/internal/validator"
@@ -12,7 +13,7 @@ import (
 var AnonymousUser = &User{}
 
 type User struct {
-	UserID    int64     `json:"user_id"`
+	ID        int64     `json:"user_id"`
 	CreatedAt time.Time `json:"created_at"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
@@ -20,6 +21,7 @@ type User struct {
 	Phone     string    `json:"phone"`
 	Activated bool      `json:"activated"`
 	Version   int       `json:"-"`
+	Deleted   bool      `json:"-"`
 }
 
 type password struct {
@@ -37,6 +39,142 @@ type UserModel struct {
 
 func (u *User) IsAnonymous() bool {
 	return u == AnonymousUser
+}
+
+func (m UserModel) Insert(user *User) error {
+	query := `
+	INSERT INTO users (name, email, phone, password_hash, activated,deleted)
+	VALUES ($1, $2, $3, $4, $5,false)
+	RETURNING id, created_at, version
+	`
+
+	args := []any{
+		user.Name,
+		user.Email,
+		user.Phone,
+		user.Password.hash,
+		user.Activated,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Version,
+	)
+
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		default:
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m UserModel) GetByEmail(email string) (*User, error) {
+	query := `
+	SELECT id, created_at, name, phone, email, password_hash, activated, version
+	FROM users
+	WHERE email = %1 AND deleted = false
+	`
+
+	var user User
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, email).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Name,
+		&user.Phone,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.Version,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &user, nil
+}
+
+func (m UserModel) Update(user *User) error {
+	query := `
+	UPDATE users SET 
+	name = $1, email = $2, phone = $3, password_hash = $4,
+	activated = $5,version = version + 1
+	WHERE id = $6 AND version = $6
+	RETURNING version`
+
+	args := []any{
+		user.Name,
+		user.Email,
+		user.Phone,
+		user.Password.hash,
+		user.Activated,
+		user.ID,
+		user.Version,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(
+		&user.Version,
+	)
+
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func (m UserModel) Delete(user *User) error {
+	query := `
+	UPDATE users set
+	deleted = true
+	where id = $1 AND version = $2
+	`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	result, err := m.DB.ExecContext(ctx, query, user.ID, user.Version)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
+	return nil
 }
 
 func ValidateEmail(v *validator.Validator, email string) {
